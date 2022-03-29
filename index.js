411a0da02ebff24017da8c3fb7efb260aa032721
Evandro Florscuk <evandro_ff@me.com>
2022-03-29 18:02:28 -0300

    Installed nodemon

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require('express')
-
 const server = express()
 
-server.get('/', () => {
-    console.log('teste')
+server.get('/home', (req, res) => {
+    return res.json({ message:`Hello World`})
 })
 
 server.listen(3000)
